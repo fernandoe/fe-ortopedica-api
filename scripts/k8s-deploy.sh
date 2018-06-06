4e6f5867f75be1e74ea0e30421f4976bf0aeab43
Fernando Espíndola <fer.esp@gmail.com>
2018-06-06 15:04:27 -0300

    Fixing the kubectl app

--- scripts/k8s-deploy.sh
+++ scripts/k8s-deploy.sh
@@ -6,4 +6,4 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 
-kubectl create namespace "${PROJECT_NAME}-${TAG//./-}"
+./kubectl create namespace "${PROJECT_NAME}-${TAG//./-}"
