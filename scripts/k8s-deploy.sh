7daa3b5b0f308a0333f48ad6f222d987a03d169a
Fernando Espíndola <fer.esp@gmail.com>
2018-06-07 10:19:51 -0300

    renaming dns name to be the namespace name

--- scripts/k8s-deploy.sh
+++ scripts/k8s-deploy.sh
@@ -12,6 +12,11 @@ if ! ./kubectl get namespace "${NAMESPACE}"; then
     ./kubectl create namespace "${NAMESPACE}"
 fi
 
+# Change version
 sed -ie 's/0.0.1/'"${TAG}"'/g' k8s/dev/ortopedica-api/3.ortopedica-api-deployment.yml
+
+# Change domain name
+sed -ie 's/ortopedica-api\.dev\.fernandoe\.com/'"${NAMESPACE}"'.dev.fernandoe.com/g' k8s/dev/ortopedica-api/5.ortopedica-api-certificate.yml
+sed -ie 's/ortopedica-api\.dev\.fernandoe\.com/'"${NAMESPACE}"'.dev.fernandoe.com/g' k8s/dev/ortopedica-api/6.ortopedica-api-ingress.yml
 
 ./kubectl apply --recursive -f ./k8s/dev/ -n ${NAMESPACE}
